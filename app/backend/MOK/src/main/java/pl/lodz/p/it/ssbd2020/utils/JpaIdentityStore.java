c3e1f5f7403a88ad017b02fcc7ac39b23ba30fbf
adrianwarcholinski <adrianwarcholinski9@gmail.com>
2020-10-24 00:45:17 +0200

    Add JpaIdentityStore :)

---
+++ app/backend/MOK/src/main/java/pl/lodz/p/it/ssbd2020/utils/JpaIdentityStore.java
@@ -0,0 +1,48 @@
+package pl.lodz.p.it.ssbd2020.utils;
+
+import pl.lodz.p.it.ssbd2020.entities.AccessLevelEntity;
+import pl.lodz.p.it.ssbd2020.entities.AccountEntity;
+import pl.lodz.p.it.ssbd2020.exceptions.AppException;
+import pl.lodz.p.it.ssbd2020.mok.facades.interfaces.AccountFacadeReadCommittedLocal;
+import pl.lodz.p.it.ssbd2020.mok.managers.AccountManager;
+import pl.lodz.p.it.ssbd2020.mok.managers.interfaces.AccountManagerLocal;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Inject;
+import javax.security.enterprise.credential.Credential;
+import javax.security.enterprise.credential.UsernamePasswordCredential;
+import javax.security.enterprise.identitystore.CredentialValidationResult;
+import javax.security.enterprise.identitystore.IdentityStore;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+@ApplicationScoped
+public class JpaIdentityStore implements IdentityStore {
+
+    @Inject
+    private AccountManagerLocal accountManager;
+
+    @Override
+    public Set<String> getCallerGroups(CredentialValidationResult validationResult) {
+        return IdentityStore.super.getCallerGroups(validationResult);
+    }
+
+    @Override
+    public CredentialValidationResult validate(Credential credential) {
+        if (credential instanceof UsernamePasswordCredential) {
+            UsernamePasswordCredential usernamePasswordCredential = (UsernamePasswordCredential) credential;
+            try {
+                AccountEntity account = accountManager.getAccountDetails(usernamePasswordCredential.getCaller());
+                Set<String> groups = account.getAccessLevels().stream()
+                        .map(AccessLevelEntity::getLevel)
+                        .collect(Collectors.toSet());
+                if (usernamePasswordCredential.compareTo(account.getLogin(), account.getPassword())) {
+                    return new CredentialValidationResult(account.getLogin(), groups);
+                }
+            } catch (AppException e) {
+                return CredentialValidationResult.INVALID_RESULT;
+            }
+        }
+        return CredentialValidationResult.NOT_VALIDATED_RESULT;
+    }
+}
