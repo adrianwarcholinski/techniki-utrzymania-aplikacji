ab73142f76385e22c2c102d61385cf3ba79085a7
FlaszKub <jakubflaszka@gmail.com>
2020-12-03 21:48:52 +0100

    logger for HealthEndpoint

--- zad_2/app/backend/common/src/main/java/pl/lodz/p/it/ssbd2020/HealthEndpoint.java
+++ zad_2/app/backend/common/src/main/java/pl/lodz/p/it/ssbd2020/HealthEndpoint.java
@@ -3,6 +3,8 @@ package pl.lodz.p.it.ssbd2020;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.core.Response;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 @Path("healthz")
 public class HealthEndpoint {
@@ -11,6 +13,7 @@ public class HealthEndpoint {
 
     @GET
     public Response isHealth() {
+        Logger.getGlobal().log(Level.INFO, "health probe {health: " + health +"}");
         if(health) {
             return Response.ok().build();
         } else {
@@ -21,6 +24,7 @@ public class HealthEndpoint {
     @GET
     @Path("change")
     public Response changeHealth() {
+        Logger.getGlobal().log(Level.INFO, "health change [health = " + health +"]");
         health = !health;
         return Response.ok().build();
     }
